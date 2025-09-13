25bcfccb80a6560ead0bf1a09993d167f573779a
SmAsHeD <6071159+smashedr@users.noreply.github.com>
2025-09-12 22:33:18 -0700

    Add @cssnr/vitepress-plugin-contributors (#4)

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -2,9 +2,12 @@ import DefaultTheme, { VPBadge } from 'vitepress/theme'
 import './custom.css'
 import 'virtual:group-icons.css'
 
-import Contributors from './components/Contributors.vue'
 import YouTubeEmbed from './components/YouTubeEmbed.vue'
 import StackTable from './components/StackTable.vue'
+
+import Contributors from '@cssnr/vitepress-plugin-contributors'
+import '@cssnr/vitepress-plugin-contributors/style.css'
+import contributors from '../contributors.json'
 
 // noinspection JSUnusedGlobalSymbols
 export default {
@@ -12,8 +15,10 @@ export default {
 
     enhanceApp({ app }) {
         app.component('Badge', VPBadge)
-        app.component('Contributors', Contributors)
         app.component('YouTubeEmbed', YouTubeEmbed)
         app.component('StackTable', StackTable)
+
+        app.component('Contributors', Contributors)
+        app.config.globalProperties.$contributors = contributors
     },
 }
