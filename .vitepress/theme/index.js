dae5301b2f78d26b35c9710354cd86b794364355
SmAsHeD <6071159+smashedr@users.noreply.github.com>
2025-09-15 00:05:43 -0700

    Add @cssnr/vitepress-plugin-copybutton (#6)

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -2,9 +2,11 @@ import DefaultTheme, { VPBadge } from 'vitepress/theme'
 import './custom.css'
 import 'virtual:group-icons.css'
 
-import CopyButton from './components/CopyButton.vue'
 import StackTable from './components/StackTable.vue'
 import YouTubeEmbed from './components/YouTubeEmbed.vue'
+
+import CopyButton from '@cssnr/vitepress-plugin-copybutton'
+import '@cssnr/vitepress-plugin-copybutton/style.css'
 
 import Contributors from '@cssnr/vitepress-plugin-contributors'
 import '@cssnr/vitepress-plugin-contributors/style.css'
@@ -16,10 +18,11 @@ export default {
 
     enhanceApp({ app }) {
         app.component('Badge', VPBadge)
-        app.component('CopyButton', CopyButton)
+
         app.component('StackTable', StackTable)
         app.component('YouTubeEmbed', YouTubeEmbed)
 
+        app.component('CB', CopyButton)
         app.component('Contributors', Contributors)
         app.config.globalProperties.$contributors = contributors
     },
